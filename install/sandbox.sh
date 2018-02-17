9470f20f340380c80cba3479a5c77b2e12c4e8a3
Petar Petrov <pppepito86@gmail.com>
2018-02-17 11:19:34 +0200

    update sandbox.sh

--- install/sandbox.sh
+++ install/sandbox.sh
@@ -2,13 +2,14 @@
 
 COMMAND=$1
 TIMEOUT=$2
+MEMORY=$3
 
 #cd "$(dirname "$0")"
 cd /shared
 
-INPUT=${3:-input}
-OUTPUT=${4:-output}
-ERROR=${5:-error}
+INPUT=${4:-input}
+OUTPUT=${5:-output}
+ERROR=${6:-error}
 
 if [ ! -f $INPUT ]; then
   INPUT=/dev/null
@@ -16,7 +17,9 @@ fi
 
 OOM_START_COUNT=$(dmesg | egrep -i 'killed process' | wc -l)
 STARTTIME=`date +%s.%N`
-timeout --signal=KILL $TIMEOUT $COMMAND < $INPUT > $OUTPUT 2> $ERROR
+ulimit -m $MEMORY
+ulimit -v $MEMORY
+timeout --signal=KILL $TIMEOUT time -p -f "%U" -o time $COMMAND < $INPUT > $OUTPUT 2> $ERROR
 echo $? > exitcode
 ENDTIME=`date +%s.%N`
 
@@ -27,7 +30,9 @@ if [ "$OOM_DIFF" -ne "0" ]; then
 fi
 
 TIMEDIFF=`echo "$ENDTIME - $STARTTIME" | bc | awk -F"." '{print $1"."substr($2,1,3)}'`
-echo $TIMEDIFF > time
+if [ ! -s time ]; then
+  echo $TIMEDIFF > time
+fi
 
 # if "exitcode" exists - the program has finished
 # or "timeout" exists - the program has timed out
