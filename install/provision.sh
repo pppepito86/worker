560139efac6b90621f1ae22c9317c7c9a8b86dca
Petar Petrov <pppepito86@gmail.com>
2019-01-04 21:10:11 +0200

    download projects before adding service

--- install/provision.sh
+++ install/provision.sh
@@ -17,17 +17,17 @@ apt-get install openjdk-8-jdk -y
 #maven
 sudo apt-get install -y maven
 
-#create start service
-cp /vagrant/worker/install/worker /etc/init.d/worker
-chmod 700 /etc/init.d/worker
-update-rc.d worker defaults
-
-#judge project
+#download projects
 git clone https://github.com/pppepito86/sandbox.git /vagrant/sandbox
 git clone https://github.com/pppepito86/grader.git /vagrant/grader
 git clone https://github.com/pppepito86/worker.git /vagrant/worker
 mvn install -f /vagrant/sandbox/pom.xml
 mvn install -f /vagrant/grader/pom.xml
 mvn install -f /vagrant/worker/pom.xml
+
+#create start service
+cp /vagrant/worker/install/worker /etc/init.d/worker
+chmod 700 /etc/init.d/worker
+update-rc.d worker defaults
 
 service worker start
