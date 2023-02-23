0c04c80797010a4ae8aec6b99c0d584ade4a1587
Petar Petrov <pppepito86@gmail.com>
2023-02-23 17:35:15 +0200

    update piper.cpp

--- src/main/resources/piper.cpp
+++ src/main/resources/piper.cpp
@@ -16,6 +16,8 @@ void error_message (string s, int code) {
 }
 
 vector <int> fds;
+vector <int> children;
+int group=-1;
 pair <int, int> make_process (char* grader) {
     pipes p;
     if ((pipe(p.pipeIn))||(pipe(p.pipeOut))) error_message("Pipe failed",-2);
@@ -33,20 +35,27 @@ pair <int, int> make_process (char* grader) {
         error_message("Exec failed",-4);
     }
     else {
+        children.push_back(pid);
+        if (group==-1) group=pid;
+        else setpgid(pid,group);
         close(p.pipeOut[0]); close(p.pipeIn[1]);
         return {p.pipeIn[0], p.pipeOut[1]};
     }
 }
+int count_digs (int num) {
+    int cnt=0;
+    for (;;) {
+        num/=10;
+        cnt++;
+        if (num==0) break;
+    }
+    return cnt;
+}
 int main (int argc, char* argv[]) {
-    if (argc<4) error_message("Arguments: number_of_grader_processes name_of_manager_program name_of_grader_program_1 ... name_of_grader_program_n",-1);
+    if (argc<4) error_message("Arguments: number_of_grader_processes name_of_manager_program name_of_grader_program",-1);
     int processes=atoi(argv[1]);
-    if ((argc>4)&&(argc!=3+processes)) error_message("If there is more than one name of grader program, the count of the names should match the number of processes",-1);
-
     signal(SIGPIPE,[] (int signal) {
-        error_message("Violation of the protocol for communication!",0);
-    });
-    signal(SIGUSR1,[] (int signal) {
-        exit(0);
+        error_message("Pipe error - violation of the protocol for communication!",0);
     });
     for (int i=0; i<processes; i++) {
         char* grader;
@@ -57,23 +66,22 @@ int main (int argc, char* argv[]) {
         fds.push_back(res.second);
     }
 
-    int pid=fork();
-    if (pid<0) error_message("Fork failed",-3);
-    else if (pid==0) {
+    int manager=fork();
+    if (manager<0) error_message("Fork failed",-3);
+    else if (manager==0) {
         char* manager=argv[2];
-        char** args=new char*[fds.size()+2];
-        args[0]=manager;
-        for (int i=0; i<fds.size(); i++) {
-            int cnt=0,tmp=fds[i];
-            for (;;) {
-                tmp/=10;
-                cnt++;
-                if (tmp==0) break;
-            }
-            args[1+i]=new char[cnt+1];
-            sprintf(args[1+i],"%d",fds[i]);
+        char** args=new char*[1+fds.size()+children.size()+1];
+        int ind=0;
+        args[ind++]=manager;
+        for (auto fd : fds) {
+            args[ind]=new char[count_digs(fd)+1];
+            sprintf(args[ind++],"%d",fd);
         }
-        args[fds.size()+1]=NULL;
+        for (auto child : children) {
+            args[ind]=new char[count_digs(child)+1];
+            sprintf(args[ind++],"%d",child);
+        }
+        args[ind++]=NULL;
         execv(manager,args);
         error_message("Exec failed",-4);
     }
@@ -81,20 +89,18 @@ int main (int argc, char* argv[]) {
         close(fd);
     }
 
-    int status;
+    int pid,status;
     while ((pid=wait(&status))>0) {
         if (WIFEXITED(status)) {
             if (WEXITSTATUS(status)) exit(WEXITSTATUS(status));
+            else if (pid==manager) {
+                kill(-group,SIGTERM);
+                exit(0);
+            }
         }
-        else if (WIFSIGNALED(status)) {
-            kill(getpid(),WTERMSIG(status));
-        }
-        else if (WIFSTOPPED(status)) {
-            kill(getpid(),WSTOPSIG(status));
-        }
-        else {
-            error_message("Unknown status of child process - "+to_string(status),-5);
-        }
+        else if (WIFSIGNALED(status)) kill(getpid(),WTERMSIG(status));
+        else if (WIFSTOPPED(status)) kill(getpid(),WSTOPSIG(status));
+        else error_message("Unknown status of child process - "+to_string(status),-5);
     }
     return 0;
 }
