b7474354448b033fdcb16a00ead9729f233d7312
root <root@ip-172-31-22-1.eu-central-1.compute.internal>
2024-02-29 15:51:51 +0000

    Small fix to startup script.

--- install/startup.sh
+++ install/startup.sh
@@ -11,7 +11,8 @@ git -C /vagrant/grader pull
 mvn install -f /vagrant/grader/pom.xml
 
 git -C /vagrant/worker pull
-if [#$ > 0]; then
+if [[ $# -gt 0 ]]
+then
 	mvn install -f /vagrant/worker/pom.xml
 else
 	mvn spring-boot:run -f /vagrant/worker/pom.xml >>/vagrant/worker/stdout 2>> /vagrant/worker/stderr &
