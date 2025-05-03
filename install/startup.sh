5252ce6325e0c4589f2dbf20c2ddff70a5d94b3f
Ubuntu <ubuntu@ip-172-31-30-196.eu-central-1.compute.internal>
2025-05-03 21:57:56 +0300

    Fixed startup script.

--- install/startup.sh
+++ install/startup.sh
@@ -13,6 +13,7 @@ mvn install -f /vagrant/grader/pom.xml
 git -C /vagrant/worker pull
 if [[ $# -gt 0 ]]
 then
+	rm /vagrant/worker/target/worker-0.0.1-SNAPSHOT.jar
 	mvn install -f /vagrant/worker/pom.xml
 else
 	mvn spring-boot:run -f /vagrant/worker/pom.xml >>/vagrant/worker/stdout 2>> /vagrant/worker/stderr &
